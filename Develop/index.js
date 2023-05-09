9e5bfe7b136239f6e6389cc097d1dff3c131e0a7
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-05-09 17:43:38 -0500

    Initial commit

---
+++ Develop/index.js
@@ -0,0 +1,55 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "Please enter a description of your project"
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "License",
+        message: ""
+    },
+    {
+        type: "input",
+        name: "Contributions",
+        message: "Contribution guidelines"
+    },
+    {
+        type: "input",
+        name: "Test",
+        message: "Test instructions"
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
