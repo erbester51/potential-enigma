8d864ad1664b576dca4d2de18554baa407edb017
erbester51 <55757768+erbester51@users.noreply.github.com>
2023-05-12 16:17:46 -0500

    Updated index.js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -16,13 +17,8 @@ const questions = [
     },
     {
         type: "input",
-        name: "Table of Contents",
-        message: ""
-    },
-    {
-        type: "input",
         name: "Installation",
-        message: ""
+        message: "What are the steps to download your application?"
     },
     {
         type: "input",
@@ -47,10 +43,16 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(readMe, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data) => {
+        fs.writeFile("README.md", generateMarkdown(data), (err) => 
+            err ? console.log(err) : console.log("Success!")
+        );
+    }
+}
 
 // Function call to initialize app
 init();
